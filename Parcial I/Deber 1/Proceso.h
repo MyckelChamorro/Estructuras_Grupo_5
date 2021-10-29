53ff9819e5026743667a9178181e3377eedfd38b
Ricardogm2000 <rsgrijalva@espe.edu.ec>
2021-10-28 22:35:25 -0500

    Proceso

---
+++ Parcial I/Deber 1/Proceso.h
@@ -0,0 +1,27 @@
+/*	UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+	DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION
+    INGENIERA DE SOFTWARE
+                
+	AUTORES:    MYCKEL CHAMORRO - mschamorro@espe.edu.ec ( LIDER )
+				RICARDO GRIJALVA - rsgrijalva@espe.edu.ec
+				CARLOS CAMPOVERDE - cdcampoverde@espe.edu.ec 
+				LIZBETH RECALDE - klcajas1@espe.edu.ec
+				
+	
+	ENUNCIADO: REALIZAR UN PROGRAMA QUE CALCULE LA OPERACION ESPECIFICADA EN CLASE Y EL MODELO REALIZADO LLEVARLO A CODIGO.				
+				
+	FECHA DE CREACION:        27-10-2021 	
+	FECHA DE MODIFICACION:    28-10-2021*/
+#include <iostream>
+class Proceso{
+	private:
+		float numerador;
+		float denominador;
+	public:
+		Proceso();
+		void setNumerador(float newNumerador);
+		void setDenominador(float newDenominador);
+		float getNumerador();
+		float getDenominador();
+		void imprimir();
+};
