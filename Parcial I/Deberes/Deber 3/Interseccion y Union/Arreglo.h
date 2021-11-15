010f4be1ea7692298633df5fa1c7a5040e464379
Ricardogm2000 <rsgrijalva@espe.edu.ec>
2021-11-14 23:44:50 -0500

    Correcion

--- Parcial I/Deberes/Deber 3/Interseccion y Union/Arreglo.h
+++ Parcial I/Deberes/Deber 3/Interseccion y Union/Arreglo.h
@@ -1,3 +1,18 @@
+/*	UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+	DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION
+    INGENIERA DE SOFTWARE
+                
+	AUTORES:    MYCKEL CHAMORRO - mschamorro@espe.edu.ec ( LIDER )
+				RICARDO GRIJALVA - rsgrijalva@espe.edu.ec
+				CARLOS CAMPOVERDE - cdcampoverde@espe.edu.ec 
+				LIZBETH RECALDE - klcajas1@espe.edu.ec
+				
+	
+	ENUNCIADO: Dividir en clases y utilizar templates en el ejercicio hecho en clase, ademas aumentar
+			   la operacion de potenciacion.				
+				
+	FECHA DE CREACION:        10-11-2021 	
+	FECHA DE MODIFICACION:    10-11-2021*/
 #pragma once
 #include <iostream>
 template<typename T>
