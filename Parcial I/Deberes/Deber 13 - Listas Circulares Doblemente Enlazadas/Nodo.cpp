2164ef8a06e6935d4a5f1dbfea3e9b6f2b89670a
StevenJossueAchig <stevenjossue5@gmail.com>
2021-11-23 23:47:24 -0500

    LCDE

--- Parcial I/Deberes/Deber 13 - Listas Circulares Doblemente Enlazadas/Nodo.cpp
+++ Parcial I/Deberes/Deber 13 - Listas Circulares Doblemente Enlazadas/Nodo.cpp
@@ -1,25 +1,26 @@
 #include "Nodo.h"
+
 Nodo::Nodo(int _dato)
 {
-dato = _dato;
-siguiente = NULL;
-anterior = NULL;
+	dato = _dato;
+	siguiente = NULL;
+	anterior = NULL;
 }
-int Nodo::getDato(){
-return dato;
+	int Nodo::getDato(){
+	return dato;
 }
-void Nodo::setDato(int _dato){
-dato = _dato;
+	void Nodo::setDato(int _dato){
+	dato = _dato;
 }
-Nodo *Nodo::getSiguiente(){
-return siguiente;
+	Nodo *Nodo::getSiguiente(){
+	return siguiente;
 }
-void Nodo::setSiguiente(Nodo *_siguiente){
-siguiente = _siguiente;
+	void Nodo::setSiguiente(Nodo *_siguiente){
+	siguiente = _siguiente;
 }
-Nodo *Nodo::getAnterior(){
-return anterior;
+	Nodo *Nodo::getAnterior(){
+	return anterior;
 }
-void Nodo::setAnterior(Nodo *_anterior){
-anterior = _anterior;
+	void Nodo::setAnterior(Nodo *_anterior){
+	anterior = _anterior;
 }
--- Parcial I/Deberes/Deber 13 - Listas Circulares Doblemente Enlazadas/Nodo.h
+++ Parcial I/Deberes/Deber 13 - Listas Circulares Doblemente Enlazadas/Nodo.h
@@ -1,20 +1,21 @@
 #ifndef NODO_H
 #define NODO_H
 #include<stdlib.h>
+
 class Nodo
 {
-public:
-Nodo(int);
-int getDato();
-void setDato(int);
-Nodo *getSiguiente();
-void setSiguiente(Nodo *);
-Nodo *getAnterior();
-void setAnterior(Nodo *);
-private:
-int dato;
-Nodo *siguiente;
-Nodo *anterior;
+	public:
+		Nodo(int);
+		int getDato();
+		void setDato(int);
+		Nodo *getSiguiente();
+		void setSiguiente(Nodo *);
+		Nodo *getAnterior();
+		void setAnterior(Nodo *);
+	private:	
+		int dato;
+		Nodo *siguiente;
+		Nodo *anterior;
 };
 #endif
 
